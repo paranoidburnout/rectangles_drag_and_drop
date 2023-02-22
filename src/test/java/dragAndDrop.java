6084e69011927acdfbd266415632b4373c3b0369
Lissa Vasenko <lisa.vasenko@outlook.com>
2023-02-22 22:31:39 +0300

    dragAndDrop.java was developed

---
+++ src/test/java/dragAndDrop.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class dragAndDrop {
+    @Test
+    void dragAndDropRectangles() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+    }
+}
